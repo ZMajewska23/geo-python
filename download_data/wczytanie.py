f18d3811aa7acd1b54b9bcb5554161bca2ce4859
pawellachowski777 <pawellachowski777@gmail.com>
2023-05-29 20:48:49 +0200

    optimalize code, add freq function

--- download_data/wczytanie.py
+++ download_data/wczytanie.py
@@ -5,7 +5,7 @@ import numpy as np
 from scipy import fft
 
 
-def downoload_data_from_files(path, day_start, day_end):
+def download_data_from_files(path, day_start, day_end):
     files = os.listdir(path)
 
     # jeżeli chcemy mieć wszystkie dane day_end podajemy None
@@ -16,12 +16,12 @@ def downoload_data_from_files(path, day_start, day_end):
     df = pd.DataFrame()
 
     for file in files:
-        nazwa = f'./data/gphone-155_2020/{file}'
+        nazwa = f'{path}/{file}'
         print(file)
         # pominięcie pierwszych linijek z metadanymi
         df_file = pd.read_csv(nazwa, sep='\t', skiprows=25)
         df_file = df_file['[DATA]'].str.split('  ', n=-1, expand=True)
-        df_file.columns = ['data', 'godzina', 'raw[nm/s2]', 'ciśnienie[mBar]', 'residua[nm/s2]']
+        df_file.columns = ['data', 'godzina', 'raw[nm/s2]', 'ciśnienie[mBar]', 'residua']
         df_file['godzina'] = df_file['godzina'].str.replace(" ", ":")
 
         df = pd.concat([df, df_file])
@@ -29,7 +29,7 @@ def downoload_data_from_files(path, day_start, day_end):
     # podział kolumn ogranicznikem '  '
     # df = df['[DATA]'].str.split('  ', n=-1, expand=True)
     # zdefiniowanie kolumn df
-    df.columns = ['data', 'godzina', 'raw[nm/s2]', 'ciśnienie[mBar]', 'residua[nm/s2]']
+    # df.columns = ['data', 'godzina', 'raw[nm/s2]', 'ciśnienie[mBar]', 'residua[nm/s2]']
 
     print('daty')
     # zamiana stringów danych na inny format
@@ -38,11 +38,11 @@ def downoload_data_from_files(path, day_start, day_end):
     df['czas'] = df['data'] + pd.to_timedelta(df['godzina'])
     df['ciśnienie[mBar]'] = df['ciśnienie[mBar]'].astype(float)
     df['raw[nm/s2]'] = df['raw[nm/s2]'].astype(float)
-    df['residua[nm/s2]'] = df['residua[nm/s2]'].astype(float)
+    df['residua'] = df['residua'].astype(float)
 
     # usuwanie błędnych danych
     # df = df[(df['residua[nm/s2]'] > 0 )&(df['residua[nm/m2]'] > 0 )]
-    df = df[(df['raw[nm/s2]'] != -9999)&(df['residua[nm/s2]'] != -9999)]
+    df = df[(df['raw[nm/s2]'] != -9999)&(df['residua'] != -9999)]
 
     print('wstawianie pustych')
     # tu wstawia NaN gdy nie ma pomiaru
@@ -73,54 +73,36 @@ def _create_dates_df_from_row(date_start, date_end):
     return df_dates
 
 
-def create_grouped_df(df, grouped_columns, new_residua):
+def create_grouped_df(df, grouped_columns):
     # # średnie residua co minute
     df_grouped = df.groupby(grouped_columns).mean(numeric_only=True).reset_index()
-    df_grouped.rename(columns={'residua[nm/s2]': new_residua}, inplace=True)
+    # df_grouped.rename(columns={'residua[nm/s2]': new_residua}, inplace=True)
 
     return df_grouped
 
-    # df_minutes = df.groupby(['data', 'godzina', 'minuta']).mean(numeric_only=True).reset_index()
-    # df_minutes.rename(columns={'residua[nm/s2]': 'residua[nm/m2]'}, inplace=True)
-    # df_minutes['czas'] = df_minutes['data'] + pd.to_timedelta(df_minutes['godzina'], 'hour') + pd.to_timedelta(df_minutes['minuta'], 'minute')
-#
-# # średnie residua co godzine
-# df_hours = df.groupby(['data', 'godzina']).mean(numeric_only = True).reset_index()
-# df_hours.rename(columns={'residua[nm/s2]': 'residua[nm/h]'}, inplace=True)
-# df_hours['czas'] = df_hours['data'] + pd.to_timedelta(df_hours['godzina'], 'hour')
-#
-#
-# # średnie residua z dni
-# df_days = df.groupby(['data']).mean(numeric_only = True).reset_index()
-# df_days.rename(columns={'residua[nm/s2]': 'residua[day]'}, inplace=True)
-# df_days['czas'] = df_days['data']
 
+def calculate_frequency(df):
+    liczba_probek = len(df['residua'])
 
-# df_month = df.groupby(['miesiac', 'rok']).mean(numeric_only = True).reset_index()
-# df_month.rename(columns={'residua[nm/s2]': 'residua[day]'}, inplace=True)
-#
-#
-# ################################################################################
-#
-# liczba_probek = len(df['residua[nm/s2]'])
-#
-# # Przeliczenie jednostek przyspieszenia z nm/s^2 na mGal
-# df['przyspieszenie'] = df['residua[nm/s2]'] / (10**4)
-# przyspieszenie = df['przyspieszenie'].values
-#
-# # Obliczenie transformaty Fouriera
-# transformaty = fft.fft(przyspieszenie)
-#
-# # Obliczenie wektora częstotliwości
-# czestotliwosci = fft.fftfreq(liczba_probek, 1/84600)
-#
-# # Znalezienie indeksu maksymalnej amplitudy
-# indeks_maks_amplitudy = np.argmax(np.abs(transformaty))
-# print('%.10f', indeks_maks_amplitudy)
-#
-# # Obliczenie częstotliwości odpowiadającej maksymalnej amplitudzie
-# czestotliwosc = czestotliwosci[indeks_maks_amplitudy]
-# print(czestotliwosc)
-#
-# df['czestotliwosc[Hz]'] = np.abs(czestotliwosc)
-# print('%.3f', df['czestotliwosc[Hz]'][100])
+    # Przeliczenie jednostek przyspieszenia z nm/s^2 na mGal
+    df['przyspieszenie'] = df['residua'] / (10**4)
+    przyspieszenie = df['przyspieszenie'].values
+
+    # Obliczenie transformaty Fouriera
+    transformaty = fft.fft(przyspieszenie)
+
+    # Obliczenie wektora częstotliwości
+    czestotliwosci = fft.fftfreq(liczba_probek, 1/84600)
+
+    # Znalezienie indeksu maksymalnej amplitudy
+    indeks_maks_amplitudy = np.argmax(np.abs(transformaty))
+    # print('%.10f', indeks_maks_amplitudy)
+
+    # Obliczenie częstotliwości odpowiadającej maksymalnej amplitudzie
+    czestotliwosc = czestotliwosci[indeks_maks_amplitudy]
+    # print(czestotliwosc)
+
+    df['czestotliwosc[Hz]'] = np.abs(czestotliwosc)
+    # print('%.3f', df['czestotliwosc[Hz]'][100])
+
+    return df
--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 import pandas as pd
-from download_data.wczytanie import downoload_data_from_files, create_grouped_df
+from download_data.wczytanie import download_data_from_files, create_grouped_df, calculate_frequency
 
-path_155 = "./data/gphone-155_2020"
+path_155 = "./data/gphone-157_2020"
 
-df = downoload_data_from_files(path_155, 0, None)
+df = download_data_from_files(path_155, 0, 10)
 
 print('grupowanie')
-df_minutes = create_grouped_df(df, ['data', 'godzina', 'minuta'], 'residua[nm/m2]')
+df_minutes = create_grouped_df(df, ['data', 'godzina', 'minuta'])
 df_minutes['czas'] = df_minutes['data'] + pd.to_timedelta(df_minutes['godzina'], 'hour') + pd.to_timedelta(df_minutes['minuta'], 'minute')
 
-
-df_hours = create_grouped_df(df, ['data', 'godzina'], 'residua[nm/h]')
+df_hours = create_grouped_df(df, ['data', 'godzina'])
 df_hours['czas'] = df_hours['data'] + pd.to_timedelta(df_hours['godzina'], 'hour')
 
-df_days = create_grouped_df(df, ['data'], 'residua[day]')
+df_days = create_grouped_df(df, ['data'])
 df_days['czas'] = df_days['data']
 
-df_month = create_grouped_df(df, ['miesiac', 'rok'], 'residua[month]')
+df_month = create_grouped_df(df, ['miesiac', 'rok'])
 
-df_minutes.to_csv('df_minutes.csv', index=False)
-df_hours.to_csv('df_hours.csv', index=False)
-df_days.to_csv('df_days.csv', index=False)
-df_month.to_csv('df_month.csv', index=False)
-
+df = calculate_frequency(df)
+print(df)
+# df.to_csv('data/df.csv', index=False)
+# df_minutes.to_csv('./data/df_minutes.csv', index=False)
+# df_hours.to_csv('./data/df_hours.csv', index=False)
+# df_days.to_csv('./data/df_days.csv', index=False)
+# df_month.to_csv('./data/df_month.csv', index=False)
